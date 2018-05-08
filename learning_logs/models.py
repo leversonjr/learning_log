1162640a2b6d4a107d10a996b7539da16e1290bc
leversonjr <38119912+leversonjr@users.noreply.github.com>
2018-05-08 00:19:26 -0300

    .

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Topic(models.Model):
     """ Um assunto sobre o qual o usuario esta aprendendo """
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete='CASCADE')
 
     def __str__(self):
         """ Devolve uma representação em string do modelo."""
@@ -22,4 +24,3 @@ class Entry(models.Model):
     def __str__(self):
         """ Devolve uma representação em string do modelo """
         return self.text [:50]  + '...'
-
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -10,20 +11,20 @@ from .forms import TopicForm, EntryForm
 def index(request):
     """ A pagina incial de Learning Log """
     return render(request, 'learning_logs/index.html')
-
+@login_required()
 def topics(request):
     """ Mostra todos os assuntos """
-    topics = Topic.objects.order_by
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
-
+@login_required()
 def topic(request, topic_id):
     """ Mostra um unico assunto e todas as suas entradas. """
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
-
+@login_required()
 def new_topic(request):
     """ Adicionar um novo assunto """
 
@@ -39,7 +40,7 @@ def new_topic(request):
             return HttpResponseRedirect(reverse('learning_logs:topics'))
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
-
+@login_required()
 def new_entry(request, topic_id):
     """ acrescenta uma nova entrada para um assunto em particular. """
     topic = Topic.objects.get(id=topic_id)
@@ -58,7 +59,7 @@ def new_entry(request, topic_id):
 
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
-
+@login_required()
 def edit_entry(request, entry_id):
     """ Edita uma entrada existente 'ENTRY' """
     entry = Entry.objects.get(id=entry_id)
@@ -76,3 +77,5 @@ def edit_entry(request, entry_id):
 
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'learning_logs/edit_entry.html', context)
+
+
